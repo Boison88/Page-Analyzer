9b988d8454706066cf63e896947de847e995c46b
boison <boison88@mail.ru>
2023-08-12 15:14:52 +0300

    2.7 edit app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,13 +1,11 @@
 from dotenv import load_dotenv
-import validators.url as url_validator
 import os
 import requests
+from validator import validate, normalize
 from db import UrlCheckDatabase, UrlDatabase
 from flask import (
     Flask,
     flash,
-    get_flashed_messages,
-    make_response,
     redirect,
     render_template,
     request,
@@ -18,6 +16,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 
 @app.route('/')
 def index():
@@ -35,15 +34,27 @@ def show_urls():
 
 @app.route('/urls', methods=['POST'])
 def post_url():
-    data = requests.form.get('url')
-    print(data)
-    if not url_validator(data):
-        flash('Некорректный URL', category='danger')
-        if len(data) > 255:
-            flash('URL превышает 255 символов', category='danger')
-        elif len(data) > 255:
-            flash('URL обязателен', category='danger')
-    return redirect(url_for('show_urls'))
+    url_address = requests.form.get('url')
+    errors = validate(url_address)
+    if errors:
+        for error in errors:
+            flash(error, 'danger')
+        return render_template('index.html',), 422
+
+    normalized_url = normalize(url_address)
+    try:
+        repo = UrlDatabase()
+        existing_record = repo.find_url_name(normalized_url)
+        if existing_record:
+            flash('Страница уже существует', 'info')
+            return redirect(url_for('show_url', id=existing_record.get('id')))
+
+        flash('Страница успешно добавлена', 'success')
+        return redirect(url_for('show_url', id=repo.save({'name': normalized_url})))
+
+    except Exception as ex:
+        flash('Error {raised_ex} while save url'.format(raised_ex=ex), 'danger')
+        return redirect(url_for('index'))
 
 
 @app.route('/urls/<int:id>', methods=['GET'])
@@ -58,10 +69,8 @@ def check_url(id):
 
 @app.errorhandler(404)
 def show_error_page(error):
-    return render_template(
-        'page404.html',
-        title='Page not found',
-    )
+    return render_template('page404.html', title='Page not found'), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
